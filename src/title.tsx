7ef4c9a43392c2296c673e7134513c2f79e987eb
sebas101-1 <79105051+sebas101-1@users.noreply.github.com>
2023-05-31 21:09:01 -0700

    added YOU

--- src/title.tsx
+++ src/title.tsx
@@ -1,32 +1,29 @@
 import './title.css';
-import Seb from "./images/profile_pic_seb.png"
+import Seb from "./images/profile_pic_seb.png";
 
 function Title() {
   return (
     <>
+      {/* Title */}
       <h1 className="title">Ballard Web Dev</h1>
+
+      {/* Slogan */}
       <p className="slogan">
         Ignite Your Online Success with <span className="edge">Cutting-Edge</span> Development
       </p>
+
+      {/* Description */}
       <div className="desc">
         <h1 className="center">About Us</h1>
         <p className="txt">
           Welcome to Web Dev Ballard, your premier web development company in Seattle's vibrant Ballard neighborhood! Our skilled team of developers, designers, and strategists is dedicated to crafting exceptional websites that align with your vision. From responsive design to seamless e-commerce integration, we offer comprehensive services to meet your needs. With a focus on client satisfaction, we prioritize open communication and collaboration. Let us transform your ideas into reality and unlock your online potential. Contact us today for exceptional web development solutions in Ballard and beyond.
         </p>
       </div>
+
+      {/* People - Sebastien */}
       <div className="people S">
         <h1 className="center">
           Sebastien
-          <a>
-            <div className="discord">
-              <img className="prof" src={Seb} alt="Seb" />
-              <div className="inline">
-                <span className="name">Seb</span>
-                <br /> {/* Add a line break here */}
-                <span className="distxt">Sebastien#2065</span>
-              </div>
-            </div>
-          </a>
         </h1>
         <p className="txt">
           I am Sebastien, a full stack web developer. With my expertise in a wide range of technologies and frameworks, I excel in creating robust and scalable web applications. I specialize in front-end development, and I am proficient in{' '}
@@ -40,7 +37,21 @@ function Title() {
           Tailwind CSS, a utility-first framework that enables me to create efficient and visually appealing designs. Combining my passion for coding with a keen eye for detail, I strive to deliver elegant solutions that enhance the user experience. With a strong foundation in both front-end and back-end development, I am dedicated to crafting high-quality software that exceeds client expectations.
         </p>
       </div>
-      <br/>
+
+      {/* People - Mathew */}
+      <div className='people M'>
+        <h1 className='center'>Mathew</h1>
+        <p className='txt'>I am a 🤓🤓🤓🤓🤓🤓</p>
+      </div>
+
+      {/* Contact */}
+      <h1 className='center'>Contact Us</h1>
+      <p className='center'>BallardWebDev@gmail.com</p> {/* you can fix this */}
+
+      <br />
+
+      {/* Footer */}
+      <h3 className='center'>Made with 💖</h3>
     </>
   );
 }
