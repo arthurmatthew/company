53e376a9a4c63bff21f87e1c4c7de08d774b5c73
notjack <89240346+nootjack@users.noreply.github.com>
2023-06-02 17:02:56 -0700

    Make base site

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Index from "./routes";
 import Layout from "./layout";
 import Title from "./title";
+import Account from "./routes/account/account";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Index />,
+  },
+  {
+    path: "/account",
+    element: <Account />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Layout>
-      <Title/>
+      {/* <Title/> */}
       <RouterProvider router={router} />
     </Layout>
   </React.StrictMode>
---
+++ src/routes/account/account.tsx
@@ -0,0 +1,7 @@
+import "./account.css";
+
+const Account = () => {
+  return <h1>Hello Account</h1>;
+};
+
+export default Account;
