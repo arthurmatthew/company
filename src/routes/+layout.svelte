4bcbed8e11cec7ed1c8e259d372f527112218668
Matthew Arthur <matthewarthur474@gmail.com>
2023-06-30 23:32:58 -0700

    Add base landing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,18 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="flex min-h-screen flex-col">
+	<header class="shrink grow-0 basis-auto">
+		<div class="border-b-2 border-slate-200 px-5 py-4">
+			<h1 class="text-3xl font-semibold">Ballard Web Dev</h1>
+		</div>
+	</header>
+	<main class="flex-auto">
+		<slot />
+	</main>
+	<footer class="flex flex-shrink basis-44">
+		<div class="w-full bg-red-500">
+			<h1>Placeholder Footer</h1>
+		</div>
+	</footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Ballard Web Dev</title>
+</svelte:head>
+
+<section id="hero" class="border-b-2 border-slate-200">
+	<div class="mx-auto max-w-5xl">
+		<div class="py-20">
+			<div class="mx-4 flex flex-col items-center justify-center">
+				<h1 class="text-center text-6xl font-black sm:text-7xl">
+					There's new <span
+						class="bg-gradient-to-tr from-purple-500 to-blue-500 bg-clip-text text-transparent"
+						>competition</span
+					> in town.
+				</h1>
+			</div>
+		</div>
+		<div class="pb-20">
+			<div class="mx-4 flex flex-col items-center justify-center">
+				<p class="text-center text-2xl">
+					Spoiler alert: <b>it's Ballard Web Dev.</b> Based out of Seattle, we want to get things
+					done. And we want to do it <b>efficiently</b>.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<style>
+	#hero {
+		background-image: url('../lib/assets/blobs.svg');
+		background-size: 100% auto;
+	}
+</style>
